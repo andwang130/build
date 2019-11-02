3e038455defb2511d549a98f20f0e984e0935ed0
andwang130 <627656470@qq.com>
2019-11-02 21:52:45 +0800

    update

---
+++ bin/www/precache-manifest.93459b10dc665854ec009495f1ad285d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "485d0955b0c7b9dc258e2996c8485759",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a834d266aff95be17d4",
+    "url": "/static/css/2.9c8377a9.chunk.css"
+  },
+  {
+    "revision": "6713a3775bbca242135a",
+    "url": "/static/css/main.6a7d7ad3.chunk.css"
+  },
+  {
+    "revision": "9a834d266aff95be17d4",
+    "url": "/static/js/2.01105ffe.chunk.js"
+  },
+  {
+    "revision": "6713a3775bbca242135a",
+    "url": "/static/js/main.b9440f5c.chunk.js"
+  },
+  {
+    "revision": "ed265ba67a33565861fe",
+    "url": "/static/js/runtime-main.13272830.js"
+  }
+]);
