56cd19636c9603ae4c7ace330f431fe7cfb5d4de
andwang130 <627656470@qq.com>
2019-11-08 23:19:41 +0800

    update

---
+++ bin/build/precache-manifest.88dc997a3af2a8d2dd780ba88410b39a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d678f5689b49f667779bd5d7106a23f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a834d266aff95be17d4",
+    "url": "/static/css/2.9c8377a9.chunk.css"
+  },
+  {
+    "revision": "b64c3805718f5df40316",
+    "url": "/static/css/main.19a4a073.chunk.css"
+  },
+  {
+    "revision": "9a834d266aff95be17d4",
+    "url": "/static/js/2.01105ffe.chunk.js"
+  },
+  {
+    "revision": "b64c3805718f5df40316",
+    "url": "/static/js/main.72f57fc3.chunk.js"
+  },
+  {
+    "revision": "ed265ba67a33565861fe",
+    "url": "/static/js/runtime-main.13272830.js"
+  }
+]);
